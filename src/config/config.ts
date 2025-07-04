04925cb538283e791f28e51961c3ef3fc3299dab
Adino Chang <adinochang@users.noreply.github.com>
2025-07-04 09:54:00 +0800

    Create config class

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface Config {
+    port: number;
+}
+
+const config: Config = {
+  port: parseInt(process.env.PORT || "3000", 10),
+};
+
+export default config;
