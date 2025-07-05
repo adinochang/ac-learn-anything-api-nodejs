b1ebf91537b0d3961a86f269a57fca88b7f83de4
Adino Chang <adinochang@users.noreply.github.com>
2025-07-05 12:24:11 +0800

    Add open AI API Key

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface Config {
-    port: number;
+  port: number;
+  openAiKey: string;
 }
 
 const config: Config = {
   port: parseInt(process.env.PORT || "3000", 10),
+  openAiKey: process.env.OPEN_AI_API_KEY || "",
 };
 
 export default config;
