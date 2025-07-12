d6197b246df2dbc07f8fec7838b7c1cde04909fa
Adino Chang <adinochang@users.noreply.github.com>
2025-07-12 11:24:42 +0800

    Fix typo

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import config from "@config/config.js";
 
-if (config.databaseUrl) {
+if (!config.databaseUrl) {
   throw new Error("DATABASE_URL is undefined");
 }
 
